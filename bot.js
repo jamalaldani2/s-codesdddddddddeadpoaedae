5d0711a049116446ead5e907aa115e320389ba66
jamalaldani2 <45242500+jamalaldani2@users.noreply.github.com>
2019-03-27 02:23:18 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -105,113 +105,7 @@ ${thisMessage}\`\`\`
     
 
 
-    client.on('message',async message => {
-
-        if(message.content.startsWith(prefix + "html")) {
-          const ayy = client.emojis.find(emoji => emoji.name === "GooD");
-const nos = client.emojis.find(emoji => emoji.name === "BaD")
-const ter = client.emojis.get("501785113814761473");
-const Devt = client.emojis.get("501785041358028811");
-const m7twa = client.emojis.get("501785575968210946");
-let customemojis = client.emojis.find(r => r.name === 'GooD');
-let customemoji = client.emojis.find(r => r.name === 'BaD');
-      if(!message.channel.guild) return message.reply('This Command For Servers Only !');
-              let staff = message.guild.member(message.author).roles.find('name' , `∘ - Support.`);
-               if(!staff) return message.channel.send(`🛑 You Cant Do This Command`) 
-        let htmlcodes = message.guild.channels.find(`name`, "web-coding");
-        if(!htmlcodes) return message.channel.send(":x:لم اجد الروم الخاص بنشر الاكواد");
-          let filter = m => m.author.id === message.author.id;
-          let thisMessage;
-          let thisFalse;
-          message.channel.send(':pencil: **| من فضلك اكتب الكود الأن... :pencil2: **').then(msg => {
-      
-          message.channel.awaitMessages(filter, {
-            max: 1,
-            time: 90000,
-            errors: ['time']
-          })
-          .then(collected => {
-            collected.first().delete();
-            thisMessage = collected.first().content;
-            let boi;
-            msg.edit(':scroll: **| من فضلك اكتب وصف الكود الأن... :pencil2: **').then(msg => {
-      
-                message.channel.awaitMessages(filter, {
-                  max: 1,
-                  time: 90000,
-                  errors: ['time']
-                })
-                .then(collected => {
-                  collected.first().delete();
-                  boi = collected.first().content;
-                  let boi2;
-                  msg.edit(':man_in_tuxedo: **| من فضلك اكتب من صنع هذا الكود الأن... :pencil2: **').then(msg => {
-      
-                    message.channel.awaitMessages(filter, {
-                      max: 1,
-                      time: 90000,
-                      errors: ['time']
-                    })
-                    .then(collected => {
-                      collected.first().delete();
-                    boi2 = collected.first().content;
-            msg.edit(':shield: **| [ هل انت متأكد من نشر الكود؟ | [ نعم ] او [ لا**');
-       message.channel.awaitMessages(response => response.content === 'نعم' || 'لا' && filter,{
-              max: 1,
-              time: 90000,
-              errors: ['time']
-            })
-            .then(collected => {
-              if(collected.first().content === 'لا') {
-                msg.delete();
-                message.delete();
-                thisFalse = false;
-              }
-              if(collected.first().content === 'نعم') {
-                if(thisFalse === false) return;
-                msg.edit(':dove: **| Done :white_check_mark:, تم بنجاح نشر كودك في روم الاكواد**');
-                collected.first().delete();
-
-                
-                htmlcodes.send(`@everyone | @here
-  =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    **${message.guild.name}© :arrow_down:**     
-    \`\`\`html
-    ${thisMessage}\`\`\`
-  =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    **وصف الكود**: ${boi}
-    **تم النشر بواسطة**: ${message.author}
-    **المصدر / الشخص الذي صنع الكود**: ${boi2}`).then(message => {
-      message.react("✅").then(() => message.react("❌"))
-    
-    
-    
-    
-                })
-
-          }
-
-        
-
-   
-
-});
-
-      });
-
-    }
-
-      );
-
-    });
-
-}
-
-);
-
-   
-  
-
+   
 
   
 
@@ -334,15 +228,11 @@ __الاوامرالخاصة__:spy::skin-tone-1:
 
 \`\`#js\`\` | نشر اكواد جافا سكريبت
 
-\`\`#py\`\` | نشر اكواد بايثون
-
-\`\`#html\`\` | نشر اكواد اتشتيامل
-
 \`\`#تقديم\`\` |
 
 \`\`#قبول\`\` |
 
-\`\`#رفض\`\` |
+\`\`#رفض\`\` 
 
 
 ∞⋅∾◅▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▻∾⋅∞
